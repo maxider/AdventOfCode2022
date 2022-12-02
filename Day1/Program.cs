574e85b6115e99b866ad51730aa08ef6252257e6
Max Weilandt <maxweilandt1998@googlemail.com>
2022-12-02 18:44:13 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,57 @@
+﻿var curDir = Directory.GetCurrentDirectory();
+var path = $"{curDir}/input.txt";
+
+var read = File.ReadLines(path);
+
+List<Elf> elfs = new List<Elf>();
+
+ElfBuilder builder = new ElfBuilder(elfs);
+
+foreach (var s in read) {
+    if (s.Equals(""))
+        builder.Build();
+    else
+        builder.Add(s);
+}
+
+
+var max = elfs.MaxBy(e => e.Total);
+elfs.Sort(new ElfComparator());
+var range = elfs.GetRange(0, 3);
+Console.WriteLine(max.Total);
+Console.WriteLine("3 biggest");
+Console.WriteLine(range.Sum(e => e.Total));
+
+public class ElfBuilder {
+    private List<string> bar = new List<string>();
+
+    private readonly List<Elf> foo;
+
+    public ElfBuilder(List<Elf> elfs) {
+        foo = elfs;
+    }
+
+    public void Add(string cal) => bar.Add(cal);
+
+    public void Build() {
+        foo.Add(new Elf(bar));
+        bar.Clear();
+    }
+}
+
+
+public struct Elf {
+    private readonly List<int> cal;
+
+    public int Total => cal.Sum();
+
+    public Elf(IEnumerable<string> values) {
+        cal = values.Select(s => int.Parse(s)).ToList();
+    }
+}
+
+public class ElfComparator : IComparer<Elf> {
+    public int Compare(Elf x, Elf y) {
+        return y.Total - x.Total;
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["Advent Of Code.csproj", "./"]
+RUN dotnet restore "Advent Of Code.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "Advent Of Code.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Advent Of Code.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Advent Of Code.dll"]
